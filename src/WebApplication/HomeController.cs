e30a1420e523fe2ee1d89b187cf0c2b9ecebf35f
gratch <valeriy@autopiter.ru>
2016-03-22 13:38:06 +0300

    git wellcome

---
+++ src/WebApplication/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication
+{
+    public class HomeController:ApiController
+    {
+        public IEnumerable<int> GetValues()
+        {
+            return Enumerable.Range(0, 20);
+        } 
+    }
+}
---
+++ src/WebApplication/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("default", "{controller}");
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            app.UseWebApi(config);
+        }
+
+
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:64650/";
+            WebApp.Start<Startup>(url: baseAddress);
+        }
+    }
+}
